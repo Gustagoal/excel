687079ea15a4292e4bea48fe71ed1dadf058e0ea
unknown <gustavocastruu@gmail.com>
2025-11-12 23:35:06 -0300

    Aprendendo a preencher as colunas/tabelas

---
+++ teste-xlsx-pandas/criando-planilha.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os 
+
+os.startfile("ficha.xlsx")
+
+# df = {"NOME":[],
+#       "IDADE":[],
+#       "PROFISSÃO":[],
+#       }
+
+# for nome in range(4):
+#     nome = input("Insira seu nome : ")
+
+# df["NOME"].append(nome)
+# df["IDADE"].append(23)
+# df["PROFISSÃO"].append("Jogador")
+
+
+# planilha = pd.DataFrame(df)
+
+# planilha.to_excel("ficha.xlsx")
+
+
